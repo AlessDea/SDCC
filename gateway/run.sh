1267e4bdbb1779102fe694d8ec4975a401fd8866
AlessDea <ale.flanker97@gmail.com>
2022-09-16 13:27:12 +0200

    added Dockerfile and everything to start a container

--- gateway/run.sh
+++ gateway/run.sh
@@ -1,4 +1,39 @@
-#!/bin/sh
-export FLASK_APP=gateway.py
-sudo -E python3 -m flask run -h 0.0.0.0 -p 80
-#python3 -m flask run -h 0.0.0.0 -p 5000
+
+############ Help function ###########
+Help(){
+	echo "Usage: ./run.sh -p [HOST PORT] -c [CONTAINER PORT] -n [NAME]"
+	echo "-p for the HOST PORT"
+	echo "-c for the CONTAINER PORT"
+	echo "-n for the container NAME"
+}
+
+
+while getopts p:c:n:h: flag
+do
+    case "${flag}" in
+        p) host_port=${OPTARG};;
+        c) container_port=${OPTARG};;
+        n) container_name=${OPTARG};;
+	h) Help
+	exit;;
+	\?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+
+    esac
+done
+
+if [[ -z $1  ||  -z $2 || -z $3 ]]
+then
+        echo "ERROR: -p, -c  and -n are mandatory arguments.";
+        echo "Usage: $0 -p [HOST PORT] -c [CONTAINER PORT] -n [NAME]"
+        echo "Try '$0 -h' for more information"
+        exit 1;
+fi
+
+echo ${host_port}
+echo ${container_port}
+echo ${container_name}
+
+docker build -t ${container_name} .
+docker run -dp ${host_port}:${container_port} ${container_name}
