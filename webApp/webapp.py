5a33cf9545b83bf6d93dde0e634305a90953d4a8
pierpaolospaziani <56635030+pierpaolospaziani@users.noreply.github.com>
2022-09-29 16:55:14 +0200

    aggiustato checkbox 'save' in newPassword

--- webApp/webapp.py
+++ webApp/webapp.py
@@ -19,7 +19,6 @@ def home():
     # name = request.cookies.get('userID')
     return render_template('home.html')
 
-
 @app.route('/newpassword/', methods=('GET', 'POST'))
 def newpassword():
     type1 = None
@@ -34,31 +33,30 @@ def newpassword():
             else:
                 symbol = True
 
-
             type = request.form['r1']
             if type != 'num':
                 type1 = request.form['r2']
-
-                service = request.form['service']
-                save = request.form['check']
-
-
-
                 if type1 == 'ulc':
                     npw = gateway_client.getNewAlphNumPw(int(len), symbol)
                 elif type1 == 'uc':
                     npw = gateway_client.getNewUpperPw(int(len), symbol)
                 else:
                     npw = gateway_client.getNewLowerPw(int(len), symbol)
-
             else:
-
                 npw = gateway_client.getNewNumPw(int(len), symbol)
 
+            service = request.form['service']
+            save = request.form.get('savePssw')
+
+            if save:
+                if not service:
+                    flash('Error: to save your password, you need to specify the liked Service!')
+                    return render_template('newPassword.html')
+                username = request.cookies.get('userID')
+                gateway_client.savePw(username, npw, service)       # Da implementare!
 
             return render_template('newPassword.html', newpasswd=npw)
     return render_template('newPassword.html')
-
 
 @app.route('/savepassword/', methods=('GET', 'POST'))
 def savepassword():
@@ -70,15 +68,12 @@ def savepassword():
             flash('Error!')
     return render_template('savePassword.html')
 
-
-
 @app.route('/newdoublecode/', methods=('GET', 'POST'))
 def newdoublecode():
     if request.method == 'POST':
         code = gateway_client.getNewNumPw(6)
         return render_template('newDoubleCode.html', newpasswd=code)
     return render_template('newDoubleCode.html')
-
 
 @app.route('/listpasswords/', methods=('GET', 'POST'))
 def listpasswords():
@@ -89,9 +84,9 @@ def listpasswords():
 
 @app.route('/getcookie')
 def getcookie():
-   name = request.cookies.get('userID')
-   email = request.cookies.get('email')
-   return '<h1>UserID = '+name+', Email = '+email+'</h1>'
+    name = request.cookies.get('userID')
+    email = request.cookies.get('email')
+    return '<h1>UserID = '+name+', Email = '+email+'</h1>'
 
 @app.route('/login/', methods=('GET', 'POST'))
 def login():
@@ -131,7 +126,6 @@ def login():
 @app.route("/logout")
 def logout():
     return render_template('homepage.html')
-
 
 @app.route('/register/', methods=('GET', 'POST'))
 def register():
